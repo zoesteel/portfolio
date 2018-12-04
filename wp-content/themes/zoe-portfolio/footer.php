a34c73eadd30d0b96994c070c5994e0f10e31d3d
zoesteel <zoeysteel@gmail.com>
2018-12-03 19:49:21 -0800

    analytics and copyright

--- wp-content/themes/zoe-portfolio/footer.php
+++ wp-content/themes/zoe-portfolio/footer.php
@@ -30,15 +30,13 @@
 
 			<div class="row">
 				<div class="col-12 site-info">
-					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'zoe-portfolio' ) ); ?>"><?php
-						/* translators: %s: CMS name, i.e. WordPress. */
-						printf( esc_html__( 'Proudly powered by %s', 'zoe-portfolio' ), 'WordPress' );
-					?></a>
-					<span class="sep"> | </span>
-					<?php
+					<hr />
+					&copy; <?php echo date("Y"); ?> Zoe Steel
+					<!-- <span class="sep"> | </span> -->
+					<!-- <?php
 						/* translators: 1: Theme name, 2: Theme author. */
 						printf( esc_html__( 'Theme: %1$s by %2$s.', 'zoe-portfolio' ), 'zoe-portfolio', '<a href="http://zoesteel.com">Zoe Steel</a>' );
-					?>
+					?> -->
 				</div><!-- .site-info -->
 			</div><!-- end row -->
 		</div><!-- end container-->
